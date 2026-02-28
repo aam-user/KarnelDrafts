06563e8ad250f9f7b82d12aae6770a057652bd01
aam_user <punktiktok@gmail.com>
2026-02-28 13:49:01 +0100

    New Araay

---
+++ Array_Paralleli/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 50
+int matricola[MAX], voto[MAX], num;
+void carica_studenti(){
+  int i;
+  printf("Qanti studenti vuoi caricare: ");
+  scanf("%d", &num);
+  for(i=0;i<num;i++){
+    printf("Inserisci matricola dell studente: ");
+    scanf("%d", &matricola[i]);
+    printf("Inserisci Voto: \n");
+    scanf("%d", &voto[i]);
+}
+}
+
+void stampa_dati (){
+    int i;
+    for(i=0;i<num;i++){
+    printf("Matricola: %d", matricola[i]);
+    printf(" Voto: %d\n", voto[i]);
+    }
+  }
+
+
+
+int main(){
+  int scelta;
+  do{
+        printf("\n====================\n");
+        printf("        MENU        \n");
+        printf("====================\n");
+        printf("1. Carica studenti\n");
+        printf("2. Stampa dati\n");
+        printf("3. Operazione 3\n");
+        printf("0. Esci\n");
+        printf("Scelta: ");
+        scanf("%d", &scelta);
+
+        switch(scelta) {
+
+            case 1: carica_studenti(); break;
+
+            case 2: stampa_dati(); break;
+
+            case 3:
+                /* CODICE OPERAZIONE 3 */
+                printf("Operazione 3 eseguita\n");
+                break;
+
+            case 0:
+                printf("Uscita...\n");
+                break;
+
+            default:
+                printf("Scelta non valida\n");
+        }
+    if (scelta != 0) {
+            printf("\t\nPremi INVIO per continuare...\n");
+            getchar(); // Mangia l'invio dello scanf
+            //getchar(); // Aspetta la pressione di un tasto
+            system("clear"); // Comando Linux per pulire lo schermo
+    }
+    } while(scelta != 0);
+
+    return 0;
+}
