f60bbe68444793d93e745bec2da737772b1c676e
aam_user <punktiktok@gmail.com>
2026-03-03 00:14:01 +0100

    New Araay

--- Esercizi/main.c
+++ Esercizi/main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
-int main (){
-    float scontoPercentuale, importoTotale,  importoSconto;
 
-    printf("Scrivi L'importo Totale ");
-    scanf("%f", &importoTotale );
-    if (importoTotale<50){
-        importoSconto=5.0;
-    }else if (importoTotale<65){
-        importoSconto=6.0;
-    }else if (importoTotale<80){
-        importoSconto=7.0;
-    }else
-        importoTotale = 10.0;
-    scontoPercentuale = importoTotale*importoSconto/100;
-    importoTotale=importoTotale-scontoPercentuale;
-    printf("%f", importoTotale);
-
+void caricavet(int dimmenzione, int  vett[]){
+  for (int i=0;i<dimmenzione;i++) {
+  scanf("%d", &vett[i]);
+  }
 }
-  
+
+int trovavet(int dimm, int vettore[], int trovato){
+  for (int i = 0;i<dimm;i++) {
+    if (trovato==vettore[i])
+    return  printf("Il numero e stato trovato in indice %d", i);
+   }
+printf("Non trovato");
+  return -1;
+}
+
+
+int main(){
+  int dim, find;
+  printf("Scrivi quanti numeri vuoi caricare\n");
+  scanf("%d", &dim);
+  int vet[dim];
+  caricavet(dim, vet);
+  printf("Scrivi numero Da trovare");
+  scanf("%d", &find);
+  trovavet(dim, vet, find);
+}
